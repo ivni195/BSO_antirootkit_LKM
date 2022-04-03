3938daba417f69bca20c595edb5e6655407ad5ef
Jakub Pilimon <pilimon.jakub@gmail.com>
2022-04-03 20:45:08 +0200

    memory scanning

--- src/core.c
+++ src/core.c
@@ -16,62 +16,15 @@ MODULE_DESCRIPTION("Antirootkit LKM.");
 MODULE_VERSION("1.0");
 
 pt_regs_t orig_kill;
-const unsigned long *module_addr_min, *module_addr_max;
 
 asmlinkage unsigned long my_kill(const struct pt_regs *regs) {
     return orig_kill(regs);
 }
-struct mod_initfree {
-    struct llist_node node;
-    void *module_init;
-};
 
 
 struct ftrace_hook hooks[] = {
         HOOK("__x64_sys_kill", my_kill, &orig_kill),
 };
-
-static int comp(const void *a, const void *b){
-    return *(unsigned long *) a > *(unsigned long *) b ? 1:-1;
-}
-
-static void get_func_assembly(char *ptr){
-    char bytes[601];
-    int i;
-
-    memzero_explicit(bytes, 601);
-
-    for(i = 0; i < 300; i++){
-        sprintf(bytes, "%s%02hhX", bytes, ptr[i]);
-    }
-    printk("%s\n", bytes);
-}
-
-
-
-
-bool check_string(char *s){
-    int i = 0;
-//    RK_INFO("---------------------------");
-
-    if (s[0] == '\0')
-        return false;
-
-    while (s[i] != '\0' && i < MODULE_NAME_LEN){
-        if(s[i] < 0x20 || s[i] >= 0x7e) {
-            return false;
-        }
-        i++;
-//        RK_INFO("%c", s[i]);
-    }
-
-    return true;
-}
-
-
-static bool is_within_mod_addr(unsigned long addr){
-    return *module_addr_min <= addr && addr <= *module_addr_max;
-}
 
 
 static int __init anti_rk_init(void) {
